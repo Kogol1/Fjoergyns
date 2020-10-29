e57e7b3cd9502fe9752be1c4a35b0a8712b2a45c
Matyas Moudry <matyas.moudry@websta.de>
2020-10-29 23:09:46 +0100

    Upgrade disk usage for multiple machines

--- app/TokenUser.php
+++ app/TokenUser.php
@@ -8,6 +8,7 @@ class TokenUser extends Model
 {
     protected $table = 'tokenmanager';
     protected $connection = 'mysql_token';
+    protected $primaryKey = 'id';
     public $timestamps = false;
     protected $fillable = [
         'name',
