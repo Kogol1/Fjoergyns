7dedc03adf4f24a7a13b112481c2ccbfdc345366
Matyas Moudry <matyas.moudry@websta.de>
2020-11-30 22:48:21 +0100

    change databases

--- config/database.php
+++ config/database.php
@@ -18,7 +18,7 @@ return [
             'host' => env('DB_HOST_LOCALHOST'),
             'database' => env('DB_DATABASE_LOCALHOST'),
             'username' => env('DB_USERNAME'),
-            'password' => env('DB_PASSWORD_DB_LOCALHOST'),
+            'password' => env('DB_PASSWORD_2'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
         ],
